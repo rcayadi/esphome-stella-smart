83eb018a152eadc974924a37120f0750846df4a5
RC <rcayadi@gmail.com>
2026-07-17 08:44:10 +0700

    Add multi-device support (ensure_list)

--- components/stella_smart/__init__.py
+++ components/stella_smart/__init__.py
@@ -35,7 +35,7 @@ StellaResetButton = stella_smart_ns.class_("StellaResetButton", button.Button)
 StellaDurationSelect = stella_smart_ns.class_("StellaDurationSelect", select.Select)
 StellaTimerSelect = stella_smart_ns.class_("StellaTimerSelect", select.Select)
 
-CONFIG_SCHEMA = (
+CONFIG_SCHEMA = cv.ensure_list(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(StellaSmartComponent),
@@ -85,45 +85,46 @@ CONFIG_SCHEMA = (
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await ble_client.register_ble_node(var, config)
+    for conf in config:
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await ble_client.register_ble_node(var, conf)
 
-    cg.add(var.set_mac_address(config[CONF_MAC_ADDRESS].as_hex))
-    cg.add(var.set_device_name(config[CONF_NAME]))
+        cg.add(var.set_mac_address(conf[CONF_MAC_ADDRESS].as_hex))
+        cg.add(var.set_device_name(conf[CONF_NAME]))
 
-    if CONF_BATTERY in config:
-        sens = await sensor.new_sensor(config[CONF_BATTERY])
-        cg.add(var.set_battery_sensor(sens))
-    if CONF_SPRAY_COUNT in config:
-        sens = await sensor.new_sensor(config[CONF_SPRAY_COUNT])
-        cg.add(var.set_spray_count_sensor(sens))
-    if CONF_POWER in config:
-        sw = await switch.new_switch(config[CONF_POWER])
-        cg.add(sw.set_parent(var))
-        cg.add(var.set_power_switch(sw))
-    if CONF_TRIGGER_SPRAY in config:
-        btn = await button.new_button(config[CONF_TRIGGER_SPRAY])
-        cg.add(btn.set_parent(var))
-        cg.add(var.set_spray_button(btn))
-    if CONF_RESET_SPRAY in config:
-        btn = await button.new_button(config[CONF_RESET_SPRAY])
-        cg.add(btn.set_parent(var))
-        cg.add(var.set_reset_button(btn))
-    if CONF_SPRAY_DURATION in config:
-        sel = await select.new_select(
-            config[CONF_SPRAY_DURATION],
-            options=["10 min", "20 min", "40 min"],
-        )
-        cg.add(sel.set_parent(var))
-        cg.add(var.set_duration_select(sel))
-    if CONF_TIMER_MODE in config:
-        sel = await select.new_select(
-            config[CONF_TIMER_MODE],
-            options=["X", "6h", "8h", "12h"],
-        )
-        cg.add(sel.set_parent(var))
-        cg.add(var.set_timer_select(sel))
-    if CONF_FIRMWARE_VERSION in config:
-        ts = await text_sensor.new_text_sensor(config[CONF_FIRMWARE_VERSION])
-        cg.add(var.set_version_sensor(ts))
+        if CONF_BATTERY in conf:
+            sens = await sensor.new_sensor(conf[CONF_BATTERY])
+            cg.add(var.set_battery_sensor(sens))
+        if CONF_SPRAY_COUNT in conf:
+            sens = await sensor.new_sensor(conf[CONF_SPRAY_COUNT])
+            cg.add(var.set_spray_count_sensor(sens))
+        if CONF_POWER in conf:
+            sw = await switch.new_switch(conf[CONF_POWER])
+            cg.add(sw.set_parent(var))
+            cg.add(var.set_power_switch(sw))
+        if CONF_TRIGGER_SPRAY in conf:
+            btn = await button.new_button(conf[CONF_TRIGGER_SPRAY])
+            cg.add(btn.set_parent(var))
+            cg.add(var.set_spray_button(btn))
+        if CONF_RESET_SPRAY in conf:
+            btn = await button.new_button(conf[CONF_RESET_SPRAY])
+            cg.add(btn.set_parent(var))
+            cg.add(var.set_reset_button(btn))
+        if CONF_SPRAY_DURATION in conf:
+            sel = await select.new_select(
+                conf[CONF_SPRAY_DURATION],
+                options=["10 min", "20 min", "40 min"],
+            )
+            cg.add(sel.set_parent(var))
+            cg.add(var.set_duration_select(sel))
+        if CONF_TIMER_MODE in conf:
+            sel = await select.new_select(
+                conf[CONF_TIMER_MODE],
+                options=["X", "6h", "8h", "12h"],
+            )
+            cg.add(sel.set_parent(var))
+            cg.add(var.set_timer_select(sel))
+        if CONF_FIRMWARE_VERSION in conf:
+            ts = await text_sensor.new_text_sensor(conf[CONF_FIRMWARE_VERSION])
+            cg.add(var.set_version_sensor(ts))
